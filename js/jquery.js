8ca039bfc101884234bc37a6da5ae15bc8bd8af7
esalas07 <estebiansalv@gmail.com>
2020-06-10 23:14:12 -0400

    Bicicleta

---
+++ js/jquery.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+        $("button").click(function() {
+            $("#hide1").hide();
+        });
+        $("button").click(function() {
+            $("#show").show();
+        });
+
+    }
+
+);
+
+
+
+$(document).ready(function() {
+        $("button").click(function() {
+            $("#hide2").hide();
+        });
+        $("button").click(function() {
+            $("#show").show();
+        });
+
+    }
+
+);
+
+
+
+
+$(document).ready(function() {
+    $("button").click(function() {
+        $(".auto").hide();
+    });
+});
+
+$(document).ready(function() {
+        $("#flip").click(function() {
+            $("#panel").slideToggle("slow");
+        });
+    }
+
+);
